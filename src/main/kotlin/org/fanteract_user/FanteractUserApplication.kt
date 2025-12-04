adbc4dcd466e22000285fdab1fefd2ef99788aff
추호성 <hosung@chuhoseong-ui-MacBookPro.local>
2025-12-04 14:39:11 +0900

    INIT: 기본 환경 변수 설치

---
+++ src/main/kotlin/org/fanteract_user/FanteractUserApplication.kt
@@ -0,0 +1,11 @@
+package org.fanteract_user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FanteractUserApplication
+
+fun main(args: Array<String>) {
+	runApplication<FanteractUserApplication>(*args)
+}
---
+++ src/test/kotlin/org/fanteract_user/FanteractUserApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.fanteract_user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FanteractUserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
